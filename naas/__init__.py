5b287c2994f22ed934d29581635957a69263c2d3
prashant1993 <prashant.cool19@gmail.com>
2014-06-07 09:30:51 +0530

    updated init.py to the new init `     def __init__(self,name,custom_Data# Please enter the commit message for your changes. Lines starting         self.name = name              # with '#' will be ignored, and an empty message aborts the commit.         self.custom_Data = custom_Data# On branch master                                       # Your branch is up-to-date with 'origin/master'.     def __repr__(self):               #         return "<Name %r>" %self.name # Changes to be committed:

--- naas/__init__.py
+++ naas/__init__.py
@@ -1,5 +1,18 @@
+# Third party lib imports
+
 from flask import Flask
 
+from sqlalchemy.engine.url import URL
+
+from flask.ext.sqlalchemy import SQLAlchemy
+
+# Local imports
+from naas import settings
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = URL(**settings.DATABASE)
+
+db = SQLAlchemy(app)
 
 import naas.views
