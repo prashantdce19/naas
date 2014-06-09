55c35e754a32c7280b84987625c180ea4864ac76
prashant1993 <prashant.cool19@gmail.com>
2014-06-09 12:13:00 +0530

    added function Developer to views.py and checked that it produced now erros while running on the flask server

--- naas/views.py
+++ naas/views.py
@@ -1,8 +1,26 @@
 from flask import request, session, g, redirect, url_for, \
      abort, render_template, flash, jsonify
 
+from datetime import datetime
+
+from flask.ext.sqlalchemy import SQLAlchemy
+#imports the modules for database, SQLAlchemy contains columns which are defined in the developer class
 from naas import app
+
+from naas import models
+from naas.models import db, Developer
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return "hello world"
+
+@app.route('/RegisterDev',methods=['GET','POST'])
+def developer():
+    if not session.get('logged_in'):
+        dev = Developer(username=username,email=email)
+        db.session.add(dev)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return redirect(url_for('index'))
